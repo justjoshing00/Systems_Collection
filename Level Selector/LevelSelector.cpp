89ccefa9ecebdf60c3724179b0134aeb5c78ba80
Josh <selmlocke@gmail.com>
2023-05-16 12:40:12 -0400

    split processes into declarations and definitions

--- Level Selector/LevelSelector.cpp
+++ Level Selector/LevelSelector.cpp
@@ -1,3 +1,22 @@
 #include "LevelSelector.h"
 
- 
+void Processes::Field(DataClasses::FieldData data)
+{
+	std::cout << "field process engaged";
+	std::cout << data.Read();
+}
+void Processes::Menu(DataClasses::MenuData data)
+{
+	std::cout << "field process engaged";
+	std::cout << data.Read();
+}
+void Processes::Combat(DataClasses::CombatData data)
+{
+	std::cout << "field process engaged";
+	std::cout << data.Read();
+}
+void Processes::Pause(DataClasses::PauseData data)
+{
+	std::cout << "field process engaged";
+	std::cout << data.Read();
+}
